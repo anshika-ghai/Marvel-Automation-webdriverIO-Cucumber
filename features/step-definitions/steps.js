6d451f76bc5c06ad6d69e12e847618ffcd84ace7
anshika-ghai <anshikaghai1501@gmail.com>
2021-10-24 14:00:42 +0100

    indent changes

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,89 +1,72 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
-const loginPage = require('../pageobjects/login.page');
-const shopPage = require('../pageobjects/shop.page');
-const ShopPage = require('../pageobjects/shop.page');
+const { Given, When, Then } = require("@cucumber/cucumber");
+const SearchPage = require("../pageobjects/search.page");
+const ShopPage = require("../pageobjects/shop.page");
 
-Given('the user is on login page', async() => { // Write code here that turns the phrase above into concrete actions
-    await loginPage.open();
-    browser.maximizeWindow();
-    // expect(browser).toHaveTitle('The Internet');
+Given("the user is on login page", async () => {
+  await SearchPage.open();
+  browser.maximizeWindow();
 });
 
-
-When('User clicks on search button', async() => {
-    // Write code here that turns the phrase above into concrete actions
-    await expect(loginPage.searchbuttonClick).toExist();
-    //await expect(loginPage.searchbuttonClick).toHaveTextContaining('Sign In');
-    await loginPage.searchbuttonClick.click();
-    //  browser.setTimeout({ 'pageLoad': 10000 })
-
+When("User clicks on search button", async () => {
+  await expect(SearchPage.searchbuttonClick).toExist();
+  await SearchPage.searchbuttonClick.click();
 });
 
-When('User Search for {string} in the COMICS section', async(avenger) => {
-    // Write code here that turns the phrase above into concrete actions  
-    // await expect(loginPage.clickComics).toExist();
-    // await expect(loginPage.clickComics).toHaveTextContaining('COMICS');
-    await loginPage.clickComics.click();
-    //await loginPage.enterAvengers.click();
-    await loginPage.enterAvengers.setValue(avenger);
-
+When("User Search for {string} in the COMICS section", async (avenger) => {
+  await SearchPage.clickComics.click();
+  await SearchPage.enterAvengers.setValue(avenger);
 });
 
-
-When('User Check and verify number of results {string}', async(number) => {
-    await expect(loginPage.suggestionOutput).toExist();
-    await loginPage.suggestionOutput.click();
-    await expect(loginPage.countOutput).toExist();
-    await expect(loginPage.countOutput).toHaveChildren({ number });
-
+When("User Check and verify number of results {string}", async (number) => {
+  await expect(SearchPage.suggestionOutput).toExist();
+  await SearchPage.suggestionOutput.click();
+  await expect(SearchPage.countOutput).toExist();
+  await expect(SearchPage.countOutput).toHaveChildren({ number });
 });
 
-When('User Check and verify number of pages {string}', async(page) => {
-    await expect(loginPage.pageNumber).toExist();
-    await expect(loginPage.pageNumber).toHaveTextContaining(page);
-
-
+When("User Check and verify number of pages {string}", async (page) => {
+  await expect(SearchPage.pageNumber).toExist();
+  await expect(SearchPage.pageNumber).toHaveTextContaining(page);
 });
 
-When('User select the {string} option', async(completeCollection) => {
-    await expect(ShopPage.moreButtonClick).toExist();
-    await ShopPage.moreButtonClick.click();
-    await expect(ShopPage.shopButtonClick).toExist();
-    await expect(ShopPage.shopButtonClick).toExist();
-    await ShopPage.shopButtonClick.click();
-    // await expect(ShopPage.completeYourCollectionOption).toExist();
-    // await expect(ShopPage.completeYourCollectionOption).toHaveTextContaining(completeCollection);
-    await ShopPage.closeModal.click();
-     await ShopPage.completeYourCollectionOption.click();
-  });
-  
-When('User filter by Age {string}', async (string)=> {
-    await expect(ShopPage.clickAge).toExist();
-    await ShopPage.clickAge.click();
-    await expect(ShopPage.filterKids).toExist();
-    await ShopPage.filterKids.click();
-  });
+When("User select the {string} option", async (completeCollection) => {
+  await expect(ShopPage.moreButtonClick).toExist();
+  await ShopPage.moreButtonClick.click();
+  await expect(ShopPage.shopButtonClick).toExist();
+  await expect(ShopPage.shopButtonClick).toExist();
+  await ShopPage.shopButtonClick.click();
+  await ShopPage.closeModal.click();
+  await ShopPage.completeYourCollectionOption.click();
+});
 
-When('User Sort by price low to high', async ()=> {
-    await ShopPage.sortByClick.click();
-    await ShopPage.sortBylowToHigh.click();
-  });
+When("User filter by Age {string}", async (string) => {
+  await expect(ShopPage.clickAge).toExist();
+  await ShopPage.clickAge.click();
+  await expect(ShopPage.filterKids).toExist();
+  await ShopPage.filterKids.click();
+});
 
-When('User Add {int} Marvel Logo Pins', async (quantity)=> {
+When("User Sort by price low to high", async () => {
+  await ShopPage.sortByClick.click();
+  await ShopPage.sortBylowToHigh.click();
+});
+
+When("User Add {int} Marvel Logo Pins", async (quantity) => {
   await expect(ShopPage.product).toExist();
   await expect(ShopPage.product).toExist();
   await ShopPage.product.click();
-  for (i=0; i<(quantity-1);i++){
-  await ShopPage.increaseQuantity.click();
+  for (i = 0; i < quantity - 1; i++) {
+    await ShopPage.increaseQuantity.click();
   }
-  });
+});
 
-When('User Check and verify at the basket side pop up that the subtotal is {string}', async (float)=> {
+When(
+  "User Check and verify at the basket side pop up that the subtotal is {string}",
+  async (float) => {
     await expect(ShopPage.addToBag).toExist();
     await ShopPage.addToBag.click();
     await expect(ShopPage.basketTotal).toExist();
     await expect(ShopPage.basketTotal).toExist();
-
-    console.log("asdfsdafffasfdsadsf"+float);
     await expect(ShopPage.basketTotal).toHaveTextContaining(float);
-  });
+  }
+);
